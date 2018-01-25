fe39f66cac8f1ac8175b50bbf6f96d3ebb3ec923
Ali Ok <aliok@apache.org>
2018-01-25 11:58:58 +0300

    Event streams vs fixed events

--- generateElasticCurl.js
+++ generateElasticCurl.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 const fixtures = require('./fixtures');
-const { buttonMetrics, sdkInitEvents } = fixtures
+const {fixedButtonMetrics, fixedSdkInitEvents} = fixtures;
 
 // clear all indices, delete all documents
 console.log(`curl -s -XDELETE 'http://localhost:9200/default-metrics'`);
@@ -50,24 +50,24 @@ curl -s -XPUT 'localhost:9200/custom-button-metrics?pretty' -H 'Content-Type: ap
 
 
 // process sdkInitEvents
-for (let sdkInitEvent of sdkInitEvents) {
-	// in every init event, we do 2 things:
-	// 1. store the sdkVersion that client uses in a non-time-series index. this is an update operation
-	// 2. simply add event to and index
+for (let sdkInitEvent of fixedSdkInitEvents) {
+    // in every init event, we do 2 things:
+    // 1. store the sdkVersion that client uses in a non-time-series index. this is an update operation
+    // 2. simply add event to and index
 
-	// operation 1
-	const usageClone = JSON.parse(JSON.stringify(sdkInitEvent));
-	const upsert = {
-		doc: usageClone,
-		"doc_as_upsert": true
-	};
-	console.log(`curl -s -XPOST 'http://localhost:9200/default-metrics/sdkVersionForClient/${sdkInitEvent.clientId}/_update' -d '${JSON.stringify(upsert)}' -H 'Content-Type: application/json'`);
+    // operation 1
+    const usageClone = JSON.parse(JSON.stringify(sdkInitEvent));
+    const upsert = {
+        doc: usageClone,
+        "doc_as_upsert": true
+    };
+    console.log(`curl -s -XPOST 'http://localhost:9200/default-metrics/sdkVersionForClient/${sdkInitEvent.clientId}/_update' -d '${JSON.stringify(upsert)}' -H 'Content-Type: application/json'`);
 
-	// operation 2
-	console.log(`curl -s -XPOST 'http://localhost:9200/default-event-metrics/initSDK/' -d '${JSON.stringify(sdkInitEvent)}' -H 'Content-Type: application/json'`);
+    // operation 2
+    console.log(`curl -s -XPOST 'http://localhost:9200/default-event-metrics/initSDK/' -d '${JSON.stringify(sdkInitEvent)}' -H 'Content-Type: application/json'`);
 }
 
 // process custom metrics. simply add them to an index
-for (let buttonMetric of buttonMetrics) {
-	console.log(`curl -s -XPOST 'http://localhost:9200/custom-button-metrics/buttonClick/' -d '${JSON.stringify(buttonMetric)}' -H 'Content-Type: application/json'`);
+for (let buttonMetric of fixedButtonMetrics) {
+    console.log(`curl -s -XPOST 'http://localhost:9200/custom-button-metrics/buttonClick/' -d '${JSON.stringify(buttonMetric)}' -H 'Content-Type: application/json'`);
 }
